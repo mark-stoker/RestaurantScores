089b61a805c49e575ca388fe73ce4df5ca7ebf50
Mark Stoker <mstoker30@googlemail.com>
2019-05-10 22:12:18 +0100

    Add honeypot field to homepage to stop bots

--- RestaurantScores/Models/Search.cs
+++ RestaurantScores/Models/Search.cs
@@ -5,5 +5,7 @@ namespace RestaurantScores.Models
 	public class Search : ISearch
 	{
 		public string SearchString { get; set; }
+		//This is a Honeypot field to stop spam
+		public string Name { get; set; }
 	}
 }
